e5fa2046404c6def4f809ed2be4d85c3efdc170a
Максим Самисько <masechk@gmail.com>
2023-02-10 15:35:11 +0300

    Modul4-4.1.8: try tu exclude OR'

--- Modul4/Program.cs
+++ Modul4/Program.cs
@@ -10,6 +10,18 @@ namespace Modul4
     {
         static void Main(string[] args)
         {
+            /// 4.1.8 try to exclude OR
+            /// 
+            int a=7, b=6;
+            bool c = (a != b) ^ (a > b); 
+            Console.WriteLine("true ^ true = {0}", c);
+            c = (a == b) ^ (a < b);
+            Console.WriteLine("false ^ false = {0}", c);
+            c = (a != b) ^ (a < b);
+            Console.WriteLine("true ^ false = {0}", c);
+            c = (a == b) ^ (a > b);
+            Console.WriteLine("false ^ true = {0}", c);
+            Console.ReadLine();
         }
     }
 }
