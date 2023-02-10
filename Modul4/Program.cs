95e820b0fbbfe7e776bdc45ed2832d366b247f50
Максим Самисько <masechk@gmail.com>
2023-02-10 15:50:52 +0300

    Modul4-4.1.15: try ternary operation

--- Modul4/Program.cs
+++ Modul4/Program.cs
@@ -12,8 +12,8 @@ namespace Modul4
         {
             /// 4.1.8 try to exclude OR
             /// 
-            int a=7, b=6;
-            bool c = (a != b) ^ (a > b); 
+            int a = 7, b = 6;
+            bool c = (a != b) ^ (a > b);
             Console.WriteLine("true ^ true = {0}", c);
             c = (a == b) ^ (a < b);
             Console.WriteLine("false ^ false = {0}", c);
@@ -21,7 +21,27 @@ namespace Modul4
             Console.WriteLine("true ^ false = {0}", c);
             c = (a == b) ^ (a > b);
             Console.WriteLine("false ^ true = {0}", c);
+
+            /// 4.1.13 try try to make logical brances
+            /// 
+            if (a==b)
+            {
+                Console.WriteLine("Condition true");
+            }
+            else if (b < 10)
+            {
+                Console.WriteLine("Condition false and b={0} < 10", b);
+            }
+            else
+            {
+                Console.WriteLine("Condition false and b={0} > 10", b);
+            }
+
+            /// 4.1.15 try ternary operation
+            var V = a != b ? a + b : b;
+            Console.WriteLine(V);
             Console.ReadLine();
+                        
         }
     }
 }
